464cac7393491b536d192f9bfaefeffde7bdc5f8
dinar wijaksono <dinarwijaksono11@gmail.com>
2022-09-03 21:09:06 +0700

    membuat migrate dan model pictiresProduc, Order, dan Chat

---
+++ app/Models/PicturesProduct.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PicturesProduct extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2022_09_03_210606_create_chats_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id');
+            $table->string('from', 30);
+            $table->string('confirmOfAttendance', 20);
+            $table->string('saying');
+            $table->integer('create_at');
+            $table->integer('update_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chats');
+    }
+}
